e8de64d3ca641593d97dc7983a9654c8e137fbdf
majki <michofilip@gmail.com>
2023-02-15 20:05:16 +0100

    controller trait

--- src/main/scala/perosnzio/controller/PersonController.scala
+++ src/main/scala/perosnzio/controller/PersonController.scala
@@ -1,6 +1,5 @@
 package perosnzio.controller
 
-import perosnzio.controller.includes.*
 import perosnzio.form.PersonForm
 import perosnzio.service.PersonService
 import zio.*
@@ -8,21 +7,21 @@ import zio.http.*
 import zio.http.model.{Method, Status}
 import zio.json.*
 
-case class PersonController(private val personService: PersonService) {
-    private val path = !! / "people"
+case class PersonController(private val personService: PersonService)
+    extends Controller {
 
     def routes = Http.collectZIO[Request] {
-        case Method.GET -> path / "stream" => personService.findAllStream.toJsonResponse
+        case Method.GET -> !! / "people" => personService.findAll.toJsonResponse
 
-        case Method.GET -> path => personService.findAll.toJsonResponse
+        case Method.GET -> !! / "people" / "stream" => personService.findAllStream.toJsonResponse
 
-        case Method.GET -> path / id => personService.findById(id.toInt).toJsonResponse
+        case Method.GET -> !! / "people" / id => personService.findById(id.toInt).toJsonResponse
 
-        case request@Method.POST -> path => request.fromJson[PersonForm](personService.create).toJsonResponse
+        case request@Method.POST -> !! / "people" => request.fromJson[PersonForm](personService.create).toJsonResponse
 
-        case request@Method.PUT -> path / id => request.fromJson[PersonForm](personService.update(id.toInt, _)).toJsonResponse
+        case request@Method.PUT -> !! / "people" / id => request.fromJson[PersonForm](personService.update(id.toInt, _)).toJsonResponse
 
-        case Method.DELETE -> path / id => personService.delete(id.toInt).toResponse(Status.NoContent)
+        case Method.DELETE -> !! / "people" / id => personService.delete(id.toInt).toResponse(Status.NoContent)
     }
 }
 
